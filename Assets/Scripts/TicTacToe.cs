5c0395ce4fc80db4353b313e751abdf8090662f4
Melody Yu <x864y008@ku.edu>
2018-11-24 01:59:55 -0600

    Audio added.

--- Assets/Scripts/TicTacToe.cs
+++ Assets/Scripts/TicTacToe.cs
@@ -94,7 +94,7 @@ public class TicTacToe : MonoBehaviour
     {
         GUIStyle fontStyle = new GUIStyle();
         fontStyle.normal.background = null;
-        fontStyle.normal.textColor = new Color(1, 1, 0);
+        fontStyle.normal.textColor = new Color(255, 255, 255);
         fontStyle.fontSize = 40;
 
         for (int i = 0; i < 3; ++i)
